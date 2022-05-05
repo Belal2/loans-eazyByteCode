de2c86d65880dca31093bfe6e7c0f5c065eca359
Belal2 <belal.ash22@gmail.com>
2022-05-05 08:30:36 +0200

    add to git

---
+++ src/main/java/com/eazybytes/loans/model/Customer.java
@@ -0,0 +1,15 @@
+package com.eazybytes.loans.model;
+
+public class Customer {
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	private int customerId;
+
+}
